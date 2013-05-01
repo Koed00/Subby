64f768e60712c1ca71e0765ac38e75b52097118f
koed00 <koed00@gmail.com>
2013-05-01 18:12:26 +0200

    more opensub work

--- SubbyGTK/Opensubtitles.org.cs
+++ SubbyGTK/Opensubtitles.org.cs
@@ -85,6 +85,80 @@ namespace SubbyGTK
 		{
 
 			public string MatchedBy{ get; set; }
+
+			public string IDSubMovieFile{ get; set; }
+
+			public string MovieHash{ get; set; }
+
+			public string MovieByteSize{ get; set; }
+
+			public string IDSubtitleFile{ get; set; }
+
+			public string SubFileName{ get; set; }
+
+			public string SubActualCD{ get; set; }
+
+			public string SubSize{ get; set; }
+
+			public string SubHash{ get; set; }
+
+			public string IDSubtitle{ get; set; }
+
+			public string UserID{ get; set; }
+
+			public string SubLanguageID{ get; set; }
+
+			public string SubFormat{ get; set; }
+
+			public string SubSumCD{ get; set; }
+
+			public string SubAuthorComment{ get; set; }
+
+			public DateTime SubAddDate{ get; set; }
+
+			public string SubBad{ get; set; }
+
+			public string SubRating{ get; set; }
+
+			public string SubDownloadsCnt{ get; set; }
+
+			public string MovieReleaseName{ get; set; }
+
+			public string IDMovie{ get; set; }
+
+			public string IDMovieImdb{ get; set; }
+
+			public string MovieName{ get; set; }
+
+			public string MovieNameEng{ get; set; }
+
+			public string MovieYear{ get; set; }
+
+			public string MovieImdbRating{ get; set; }
+
+			public string SubFeatured{ get; set; }
+
+			public string UserNickName{ get; set; }
+
+			public string ISO639{ get; set; }
+
+			public string LanguageName{ get; set; }
+
+			public string SubComments{ get; set; }
+
+			public string SubHearingImpaired{ get; set; }
+
+			public string UserRank{ get; set; }
+
+			public string SeriesSeason{ get; set; }
+
+			public string SeriesEpisode{ get; set; }
+
+			public string MovieKind{ get; set; }
+
+			public string SubDownloadLink{ get; set; }
+
+			public string ZipDownloadLink{ get; set; }
 		}
 
 		public  List<SearchResult> ParseSearchResult (Hashtable result)
@@ -93,7 +167,44 @@ namespace SubbyGTK
 			var ret = new List<SearchResult> ();
 			foreach (Hashtable res in (ArrayList)result["data"]) {
 				var p = new SearchResult ();
-				p.MatchedBy = res ["MatchedBy"].ToString ();
+				p.MatchedBy = (res ["MatchedBy"] ?? string.Empty).ToString ();
+				p.IDSubMovieFile = (res ["IDSubMovieFile"] ?? string.Empty).ToString ();
+				p.MovieHash = (res ["MovieHash"] ?? string.Empty).ToString ();
+				p.MovieByteSize = (res ["MovieByteSize"] ?? string.Empty).ToString ();
+				p.IDSubtitleFile = (res ["IDSubtitleFile"] ?? string.Empty).ToString ();
+				p.SubActualCD = (res ["SubActualCD"] ?? string.Empty).ToString ();
+				p.SubSize = (res ["SubSize"] ?? string.Empty).ToString ();
+				p.SubHash = (res ["SubHash"] ?? string.Empty).ToString ();
+				p.IDSubtitle = (res ["IDSubtitle"] ?? string.Empty).ToString ();
+				p.SubFileName = (res ["SubFileName"] ?? string.Empty).ToString ();
+				p.UserID = (res ["UserID"] ?? string.Empty).ToString ();
+				p.SubLanguageID = (res ["SubLanguageID"] ?? string.Empty).ToString ();
+				p.SubFormat = (res ["SubFormat"] ?? string.Empty).ToString ();
+				p.SubSumCD = (res ["SubSumCD"] ?? string.Empty).ToString ();
+				p.SubAuthorComment = (res ["SubAuthorComment"] ?? string.Empty).ToString ();
+				p.SubAddDate = DateTime.Parse (res ["SubAddDate"].ToString());
+				p.SubBad = (res ["SubBad"] ?? string.Empty).ToString ();
+				p.SubRating = (res ["SubRating"] ?? string.Empty).ToString ();
+				p.SubDownloadsCnt = (res ["SubDownloadsCnt"] ?? string.Empty).ToString ();
+				p.MovieReleaseName = (res ["MovieReleaseName"] ?? string.Empty).ToString ();
+				p.IDMovie = (res ["IDMovie"] ?? string.Empty).ToString ();
+				p.IDMovieImdb = (res ["IDMovieImdb"] ?? string.Empty).ToString ();
+				p.MovieName = (res ["MovieName"] ?? string.Empty).ToString ();
+				p.MovieNameEng = (res ["MovieNameEng"] ?? string.Empty).ToString ();
+				p.MovieYear = (res ["MovieYear"] ?? string.Empty).ToString ();
+				p.MovieImdbRating = (res ["MovieImdbRating"] ?? string.Empty).ToString ();
+				p.SubFeatured = (res ["SubFeatured"] ?? string.Empty).ToString ();
+				p.UserNickName = (res ["UserNickName"] ?? string.Empty).ToString ();
+				p.ISO639 = (res ["ISO639"] ?? string.Empty).ToString ();
+				p.LanguageName = (res ["LanguageName"] ?? string.Empty).ToString ();
+				p.SubComments = (res ["SubComments"] ?? string.Empty).ToString ();
+				p.SubHearingImpaired = (res ["SubHearingImpaired"] ?? string.Empty).ToString ();
+				p.UserRank = (res ["UserRank"] ?? string.Empty).ToString ();
+				p.SeriesSeason = (res ["SeriesSeason"] ?? string.Empty).ToString ();
+				p.SeriesEpisode = (res ["SeriesEpisode"] ?? string.Empty).ToString ();
+				p.MovieKind = (res ["MovieKind"] ?? string.Empty).ToString ();
+				p.SubDownloadLink = (res ["SubDownloadLink"] ?? string.Empty).ToString ();
+				p.ZipDownloadLink = (res ["ZipDownloadLink"] ?? string.Empty).ToString ();
 				ret.Add (p);
 			}	
 
