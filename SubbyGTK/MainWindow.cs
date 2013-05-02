483b907de8667bf2949a33ddcca797186c9d1dad
Ilan Steemers <koed00@gmail.com>
2013-05-02 16:21:23 +0200

    Cleaned up the code a bit

--- SubbyGTK/MainWindow.cs
+++ SubbyGTK/MainWindow.cs
@@ -1,151 +1,147 @@
 using System;
-using Gtk;
-using SubbyGTK;
-using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
-using System.Net;
 using System.IO;
 using System.IO.Compression;
+using System.Net;
+using SubbyGTK;
 
-public partial class MainWindow: Gtk.Window
-{	
-	private	string fname = string.Empty;
+public partial class MainWindow : Gtk.Window
+{
+    private string fname = string.Empty;
 
-	public MainWindow (): base (Gtk.WindowType.Toplevel)
-	{
-		Build ();
-		Gtk.TreeViewColumn movieColumn = new Gtk.TreeViewColumn ();
-		movieColumn.Title = "Movie";
-		Gtk.CellRendererText movieNameCell = new Gtk.CellRendererText ();
-		movieColumn.PackStart (movieNameCell, true);
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        var movieColumn = new Gtk.TreeViewColumn();
+        movieColumn.Title = "Movie";
+        var movieNameCell = new Gtk.CellRendererText();
+        movieColumn.PackStart(movieNameCell, true);
 
-		Gtk.TreeViewColumn yearColumn = new Gtk.TreeViewColumn ();
-		yearColumn.Title = "Year";
-		Gtk.CellRendererText yearTitleCell = new Gtk.CellRendererText ();
-		yearColumn.PackStart (yearTitleCell, true);
+        var yearColumn = new Gtk.TreeViewColumn();
+        yearColumn.Title = "Year";
+        var yearTitleCell = new Gtk.CellRendererText();
+        yearColumn.PackStart(yearTitleCell, true);
 
-		Gtk.TreeViewColumn seasonColumn = new Gtk.TreeViewColumn ();
-		seasonColumn.Title = "Season";
-		Gtk.CellRendererText seasonTitleCell = new Gtk.CellRendererText ();
-		seasonColumn.PackStart (seasonTitleCell, true);
+        var seasonColumn = new Gtk.TreeViewColumn();
+        seasonColumn.Title = "Season";
+        var seasonTitleCell = new Gtk.CellRendererText();
+        seasonColumn.PackStart(seasonTitleCell, true);
 
-		Gtk.TreeViewColumn episodeColumn = new Gtk.TreeViewColumn ();
-		episodeColumn.Title = "Episode";
-		Gtk.CellRendererText episodeTitleCell = new Gtk.CellRendererText ();
-		episodeColumn.PackStart (episodeTitleCell, true);
+        var episodeColumn = new Gtk.TreeViewColumn();
+        episodeColumn.Title = "Episode";
+        var episodeTitleCell = new Gtk.CellRendererText();
+        episodeColumn.PackStart(episodeTitleCell, true);
 
-		Gtk.TreeViewColumn ratingColumn = new Gtk.TreeViewColumn ();
-		ratingColumn.Title = "Rating";
-		Gtk.CellRendererText ratingTitleCell = new Gtk.CellRendererText ();
-		ratingColumn.PackStart (ratingTitleCell, true);
+        var ratingColumn = new Gtk.TreeViewColumn();
+        ratingColumn.Title = "Rating";
+        var ratingTitleCell = new Gtk.CellRendererText();
+        ratingColumn.PackStart(ratingTitleCell, true);
 
-		tree.AppendColumn (movieColumn);
-		tree.AppendColumn (yearColumn);
-		tree.AppendColumn (seasonColumn);
-		tree.AppendColumn (episodeColumn);
-		tree.AppendColumn (ratingColumn);
+        tree.AppendColumn(movieColumn);
+        tree.AppendColumn(yearColumn);
+        tree.AppendColumn(seasonColumn);
+        tree.AppendColumn(episodeColumn);
+        tree.AppendColumn(ratingColumn);
 
-		movieColumn.AddAttribute (movieNameCell, "text", 0);
-		yearColumn.AddAttribute (yearTitleCell, "text", 1);
-		seasonColumn.AddAttribute (seasonTitleCell, "text", 2);
-		episodeColumn.AddAttribute (episodeTitleCell, "text", 3);
-		ratingColumn.AddAttribute (ratingTitleCell, "text", 4);
+        movieColumn.AddAttribute(movieNameCell, "text", 0);
+        yearColumn.AddAttribute(yearTitleCell, "text", 1);
+        seasonColumn.AddAttribute(seasonTitleCell, "text", 2);
+        episodeColumn.AddAttribute(episodeTitleCell, "text", 3);
+        ratingColumn.AddAttribute(ratingTitleCell, "text", 4);
+    }
 
+    public void PushStatus(uint i, string statustext)
+    {
+        statusbar1.Push(i, statustext);
+    }
 
-	}
-	public void PushStatus(uint i, string statustext){
+    public void PopulateLanguages(string deflang)
+    {
+        var opensub = new OpenSubtitlesClient();
+        ArrayList langs = opensub.GetSubLanguages();
+        var langstore = new Gtk.ListStore(typeof (string), typeof (string));
+        foreach (Hashtable lang in langs)
+        {
+            langstore.AppendValues(lang["LanguageName"].ToString(), (lang["ISO639"] ?? string.Empty).ToString());
+        }
+        combobox2.Model = langstore;
+        statusbar1.Push(4, "Ready.");
+    }
 
-		statusbar1.Push (i, statustext);
-	}
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
 
-	public void PopulateLanguages(string deflang){
+    protected void OnButton1Clicked(object sender, EventArgs e)
+    {
+        var chooser = new FileChooserDialog(
+            "Please select movie...",
+            this,
+            FileChooserAction.Open,
+            "Cancel", ResponseType.Cancel,
+            "Select", ResponseType.Accept);
+        if (chooser.Run() == (int) ResponseType.Accept)
+        {
+            fname = chooser.Filename;
+            entry2.Text = fname;
+        }
+        chooser.Destroy();
+        statusbar1.Push(1, "Searching for filename.");
 
-		var opensub = new OpenSubtitlesClient ();
-		var langs = opensub.GetSubLanguages ();
-		var langstore = new Gtk.ListStore (typeof (string), typeof(string));
-		foreach (Hashtable lang in langs) {
+        var musicListStore = new Gtk.TreeStore(typeof (string), typeof (string), typeof (string), typeof (string),
+                                               typeof (string), typeof (string));
+        var opensub = new OpenSubtitlesClient();
+        List<OpenSubtitlesClient.SearchResult> subtitles = opensub.FileSearch(fname);
+        statusbar1.Push(2, "Found " + subtitles.Count + " titles");
 
-			langstore.AppendValues (lang["LanguageName"].ToString(), (lang ["ISO639"] ?? string.Empty).ToString ());
+        Gtk.TreeIter iter;
+        foreach (OpenSubtitlesClient.SearchResult sub in subtitles)
+        {
+            iter = musicListStore.AppendValues(sub.MovieName, sub.MovieYear, sub.SeriesSeason, sub.SeriesEpisode,
+                                               sub.SubRating, sub.SubDownloadLink);
 
-		}
-		combobox2.Model = langstore;
-		statusbar1.Push (4, "Ready.");
-	}
-	
-	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
-	{
-		Application.Quit ();
-		a.RetVal = true;
-	}
+            musicListStore.AppendValues(iter, "Author Comment:", sub.SubAuthorComment);
+            musicListStore.AppendValues(iter, "Language", sub.LanguageName);
+        }
+        tree.Model = musicListStore;
+        //tree.ExpandAll ();
+    }
 
-	protected void OnButton1Clicked (object sender, EventArgs e)
-	{
-		FileChooserDialog chooser = new FileChooserDialog (
-			"Please select movie...",
-			this,
-			FileChooserAction.Open,
-			"Cancel", ResponseType.Cancel,
-			"Select", ResponseType.Accept);
-		if (chooser.Run () == (int)ResponseType.Accept) {
+    protected void OnButton2Clicked(object sender, EventArgs e)
+    {
+        TreeIter iter;
+        tree.Selection.GetSelected(out iter);
+        var downloadlinkg = (string) tree.Model.GetValue(iter, 5);
+        string movietitle = Path.GetFileNameWithoutExtension(fname);
+        var Client = new WebClient();
+        string filename = "/tmp/tmp_" + movietitle + ".gz";
+        Client.DownloadFile(downloadlinkg, filename);
+        var fileToDecompress = new FileInfo(filename);
+        string currentFileName = fileToDecompress.FullName;
+        string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
 
-			fname = chooser.Filename;
-			entry2.Text = fname;
-		
-		} 
-		chooser.Destroy ();
-		statusbar1.Push (1, "Searching for filename.");
-	
-		Gtk.TreeStore musicListStore = new Gtk.TreeStore (typeof (string), typeof(string), typeof(string), typeof(string), typeof(string), typeof(string));
-		var opensub = new OpenSubtitlesClient ();
-		var subtitles = opensub.FileSearch (fname);
-		statusbar1.Push (2, "Found " + subtitles.Count + " titles");
+        using (FileStream originalFileStream = fileToDecompress.OpenRead())
+        {
+            using (FileStream decompressedFileStream = File.Create(newFileName))
+            {
+                using (var decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                {
+                    decompressionStream.CopyTo(decompressedFileStream);
+                }
+            }
+        }
+        fileToDecompress.Delete();
+        string origlocation = Path.GetDirectoryName(fname);
+        string newfile = origlocation + "/" + movietitle + ".srt"; //TODO add actual extension type
+        if (File.Exists(newfile))
+        {
+            File.Delete(newfile);
+        } //TODO: ask for overwrite?
 
-		Gtk.TreeIter iter;
-		foreach (var sub in subtitles) {
-			iter = musicListStore.AppendValues (sub.MovieName, sub.MovieYear, sub.SeriesSeason, sub.SeriesEpisode, sub.SubRating, sub.SubDownloadLink);
-
-			musicListStore.AppendValues (iter, "Author Comment:", sub.SubAuthorComment);
-			musicListStore.AppendValues (iter, "Language", sub.LanguageName);
-
-
-		}
-		tree.Model = musicListStore;
-		//tree.ExpandAll ();
-
-	}
-
-	protected void OnButton2Clicked (object sender, EventArgs e)
-	{
-		TreeIter iter;
-		tree.Selection.GetSelected (out iter);
-		var downloadlinkg = (string)tree.Model.GetValue (iter, 5);
-		var movietitle = System.IO.Path.GetFileNameWithoutExtension (fname);
-		WebClient Client = new WebClient ();
-		var filename = "/tmp/tmp_" + movietitle + ".gz";
-		Client.DownloadFile (downloadlinkg, filename);
-		var fileToDecompress = new FileInfo (filename);
-		string currentFileName = fileToDecompress.FullName;
-		string newFileName = currentFileName.Remove (currentFileName.Length - fileToDecompress.Extension.Length);
-
-		using (FileStream originalFileStream = fileToDecompress.OpenRead()) {
-			using (FileStream decompressedFileStream = File.Create(newFileName)) {
-				using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress)) {
-					decompressionStream.CopyTo (decompressedFileStream);
-
-				}
-			}
-		}
-		fileToDecompress.Delete ();
-		string origlocation = System.IO.Path.GetDirectoryName (fname);
-		string newfile = origlocation + "/" + movietitle + ".srt"; //TODO add actual extension type
-		if (File.Exists (newfile)) {
-			File.Delete (newfile);
-		} //TODO: ask for overwrite?
-
-		File.Move (newFileName, newfile);
-		statusbar1.Push (3, "Downloaded " + newfile+". Done");
-
-
-	}
+        File.Move(newFileName, newfile);
+        statusbar1.Push(3, "Downloaded " + newfile + ". Done");
+    }
 }
--- SubbyGTK/Opensubtitles.org.cs
+++ SubbyGTK/Opensubtitles.org.cs
@@ -2,218 +2,215 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using SubbyGTK.XmlRpc;
 
 namespace SubbyGTK
 {
-	public class OpenSubtitlesClient
-	{
-		private const string useragent = "Subby";
-		private const string apiurl = "http://api.opensubtitles.org/xml-rpc";
-		private string token;
+    public class OpenSubtitlesClient
+    {
+        private const string Useragent = "Subby";
+        private const string Apiurl = "http://api.opensubtitles.org/xml-rpc";
+        private readonly string _token;
 
-		public OpenSubtitlesClient ()
-		{
-			token = Login ();
-		}
+        public OpenSubtitlesClient()
+        {
+            _token = Login();
+        }
 
-		public string Login ()
-		{
-			var client = new XmlRpc.XmlRpcRequest ();
-			client.MethodName = "LogIn";
-			client.Params.Add ("");
-			client.Params.Add ("");
-			client.Params.Add ("en");
-			client.Params.Add (useragent);
-			var result = (Hashtable)client.Invoke (apiurl);
-			if (result ["status"].ToString () == "200 OK") {
-				return result ["token"].ToString ();
-			}
-			return string.Empty;
-		}
+        public string Login()
+        {
+            var client = new XmlRpcRequest {MethodName = "LogIn"};
+            client.Params.Add("");
+            client.Params.Add("");
+            client.Params.Add("en");
+            client.Params.Add(Useragent);
+            var result = (Hashtable) client.Invoke(Apiurl);
+            if (result["status"].ToString() == "200 OK")
+            {
+                return result["token"].ToString();
+            }
+            return string.Empty;
+        }
 
-		public bool Logout ()
-		{
-			if (string.IsNullOrEmpty (token))
-				return false;
-			var client = new XmlRpc.XmlRpcRequest ();
-			client.MethodName = "LogOut";
-			client.Params.Add (token);
-			client.Params.Add (useragent);
-			var result = (Hashtable)client.Invoke (apiurl);
-			return(result ["status"].ToString () == "200 OK");
-		}
+        public bool Logout()
+        {
+            if (string.IsNullOrEmpty(_token))
+                return false;
+            var client = new XmlRpcRequest {MethodName = "LogOut"};
+            client.Params.Add(_token);
+            client.Params.Add(Useragent);
+            var result = (Hashtable) client.Invoke(Apiurl);
+            return (result["status"].ToString() == "200 OK");
+        }
 
-		public List<SearchResult> FileSearch (string filename)
-		{
-			var client = new XmlRpc.XmlRpcRequest ();
-			client.MethodName = "SearchSubtitles";
-			client.Params.Add (token);
-			var query = new ArrayList ();
-			var qhash = new Hashtable ();
-			var moviehash = MovieHasher.GetHash (filename);
-			var moviebytes = MovieHasher.GetByteSize (filename);
-			qhash.Add ("moviehash", moviehash);
-			qhash.Add ("moviebytesize", moviebytes);
-			query.Add (qhash);
-			client.Params.Add (query);
-			var result = (Hashtable)client.Invoke (apiurl);
-			bool hasdata = false;
-			if (bool.TryParse (result ["data"].ToString(), out hasdata))
-				return TitleSearch (filename);
-			return ParseSearchResult (result);
+        public List<SearchResult> FileSearch(string filename)
+        {
+            var client = new XmlRpcRequest {MethodName = "SearchSubtitles"};
+            client.Params.Add(_token);
+            var query = new ArrayList();
+            var qhash = new Hashtable();
+            string moviehash = MovieHasher.GetHash(filename);
+            long moviebytes = MovieHasher.GetByteSize(filename);
+            qhash.Add("moviehash", moviehash);
+            qhash.Add("moviebytesize", moviebytes);
+            query.Add(qhash);
+            client.Params.Add(query);
+            var result = (Hashtable) client.Invoke(Apiurl);
+            bool hasdata;
+            if (bool.TryParse(result["data"].ToString(), out hasdata))
+                return TitleSearch(filename);
+            return ParseSearchResult(result);
+        }
 
-		}
+        public ArrayList GetSubLanguages()
+        {
+            var client = new XmlRpcRequest {MethodName = "GetSubLanguages"};
+            var result = (Hashtable) client.Invoke(Apiurl);
+            return (ArrayList) result["data"];
+        }
 
-		public ArrayList GetSubLanguages(){
-			var client = new XmlRpc.XmlRpcRequest ();
-			client.MethodName = "GetSubLanguages";
-			var result = (Hashtable)client.Invoke (apiurl);
-			return (ArrayList)result["data"];
-		}
-		public  List<SearchResult> TitleSearch (string filename)
-		{
-			var client = new XmlRpc.XmlRpcRequest ();
-			client.MethodName = "SearchSubtitles";
-			client.Params.Add (token);
-			var query = new ArrayList ();
-			var qhash = new Hashtable ();
-			var movietitle = Path.GetFileNameWithoutExtension (filename);
-			qhash.Add ("query", movietitle);
-			query.Add (qhash);
-			client.Params.Add (query);
-			var result = (Hashtable)client.Invoke (apiurl);
-			return ParseSearchResult (result);
-		}
+        public List<SearchResult> TitleSearch(string filename)
+        {
+            var client = new XmlRpcRequest {MethodName = "SearchSubtitles"};
+            client.Params.Add(_token);
+            var query = new ArrayList();
+            var qhash = new Hashtable();
+            string movietitle = Path.GetFileNameWithoutExtension(filename);
+            qhash.Add("query", movietitle);
+            query.Add(qhash);
+            client.Params.Add(query);
+            var result = (Hashtable) client.Invoke(Apiurl);
+            return ParseSearchResult(result);
+        }
 
-		public class SearchResult
-		{
+        public List<SearchResult> ParseSearchResult(Hashtable result)
+        {
+            return (from Hashtable res in (ArrayList) result["data"]
+                    select new SearchResult
+                               {
+                                   MatchedBy = (res["MatchedBy"] ?? string.Empty).ToString(),
+                                   IDSubMovieFile = (res["IDSubMovieFile"] ?? string.Empty).ToString(),
+                                   MovieHash = (res["MovieHash"] ?? string.Empty).ToString(),
+                                   MovieByteSize = (res["MovieByteSize"] ?? string.Empty).ToString(),
+                                   IDSubtitleFile = (res["IDSubtitleFile"] ?? string.Empty).ToString(),
+                                   SubActualCD = (res["SubActualCD"] ?? string.Empty).ToString(),
+                                   SubSize = (res["SubSize"] ?? string.Empty).ToString(),
+                                   SubHash = (res["SubHash"] ?? string.Empty).ToString(),
+                                   IDSubtitle = (res["IDSubtitle"] ?? string.Empty).ToString(),
+                                   SubFileName = (res["SubFileName"] ?? string.Empty).ToString(),
+                                   UserID = (res["UserID"] ?? string.Empty).ToString(),
+                                   SubLanguageID = (res["SubLanguageID"] ?? string.Empty).ToString(),
+                                   SubFormat = (res["SubFormat"] ?? string.Empty).ToString(),
+                                   SubSumCD = (res["SubSumCD"] ?? string.Empty).ToString(),
+                                   SubAuthorComment = (res["SubAuthorComment"] ?? string.Empty).ToString(),
+                                   SubAddDate = DateTime.Parse(res["SubAddDate"].ToString()),
+                                   SubBad = (res["SubBad"] ?? string.Empty).ToString(),
+                                   SubRating = (res["SubRating"] ?? string.Empty).ToString(),
+                                   SubDownloadsCnt = (res["SubDownloadsCnt"] ?? string.Empty).ToString(),
+                                   MovieReleaseName = (res["MovieReleaseName"] ?? string.Empty).ToString(),
+                                   IDMovie = (res["IDMovie"] ?? string.Empty).ToString(),
+                                   IDMovieImdb = (res["IDMovieImdb"] ?? string.Empty).ToString(),
+                                   MovieName = (res["MovieName"] ?? string.Empty).ToString(),
+                                   MovieNameEng = (res["MovieNameEng"] ?? string.Empty).ToString(),
+                                   MovieYear = (res["MovieYear"] ?? string.Empty).ToString(),
+                                   MovieImdbRating = (res["MovieImdbRating"] ?? string.Empty).ToString(),
+                                   SubFeatured = (res["SubFeatured"] ?? string.Empty).ToString(),
+                                   UserNickName = (res["UserNickName"] ?? string.Empty).ToString(),
+                                   ISO639 = (res["ISO639"] ?? string.Empty).ToString(),
+                                   LanguageName = (res["LanguageName"] ?? string.Empty).ToString(),
+                                   SubComments = (res["SubComments"] ?? string.Empty).ToString(),
+                                   SubHearingImpaired = (res["SubHearingImpaired"] ?? string.Empty).ToString(),
+                                   UserRank = (res["UserRank"] ?? string.Empty).ToString(),
+                                   SeriesSeason = (res["SeriesSeason"] ?? string.Empty).ToString(),
+                                   SeriesEpisode = (res["SeriesEpisode"] ?? string.Empty).ToString(),
+                                   MovieKind = (res["MovieKind"] ?? string.Empty).ToString(),
+                                   SubDownloadLink = (res["SubDownloadLink"] ?? string.Empty).ToString(),
+                                   ZipDownloadLink = (res["ZipDownloadLink"] ?? string.Empty).ToString()
+                               }).ToList();
+        }
 
-			public string MatchedBy{ get; set; }
+        #region Nested type: SearchResult
 
-			public string IDSubMovieFile{ get; set; }
+        public class SearchResult
+        {
+            public string MatchedBy { get; set; }
 
-			public string MovieHash{ get; set; }
+            public string IDSubMovieFile { get; set; }
 
-			public string MovieByteSize{ get; set; }
+            public string MovieHash { get; set; }
 
-			public string IDSubtitleFile{ get; set; }
+            public string MovieByteSize { get; set; }
 
-			public string SubFileName{ get; set; }
+            public string IDSubtitleFile { get; set; }
 
-			public string SubActualCD{ get; set; }
+            public string SubFileName { get; set; }
 
-			public string SubSize{ get; set; }
+            public string SubActualCD { get; set; }
 
-			public string SubHash{ get; set; }
+            public string SubSize { get; set; }
 
-			public string IDSubtitle{ get; set; }
+            public string SubHash { get; set; }
 
-			public string UserID{ get; set; }
+            public string IDSubtitle { get; set; }
 
-			public string SubLanguageID{ get; set; }
+            public string UserID { get; set; }
 
-			public string SubFormat{ get; set; }
+            public string SubLanguageID { get; set; }
 
-			public string SubSumCD{ get; set; }
+            public string SubFormat { get; set; }
 
-			public string SubAuthorComment{ get; set; }
+            public string SubSumCD { get; set; }
 
-			public DateTime SubAddDate{ get; set; }
+            public string SubAuthorComment { get; set; }
 
-			public string SubBad{ get; set; }
+            public DateTime SubAddDate { get; set; }
 
-			public string SubRating{ get; set; }
+            public string SubBad { get; set; }
 
-			public string SubDownloadsCnt{ get; set; }
+            public string SubRating { get; set; }
 
-			public string MovieReleaseName{ get; set; }
+            public string SubDownloadsCnt { get; set; }
 
-			public string IDMovie{ get; set; }
+            public string MovieReleaseName { get; set; }
 
-			public string IDMovieImdb{ get; set; }
+            public string IDMovie { get; set; }
 
-			public string MovieName{ get; set; }
+            public string IDMovieImdb { get; set; }
 
-			public string MovieNameEng{ get; set; }
+            public string MovieName { get; set; }
 
-			public string MovieYear{ get; set; }
+            public string MovieNameEng { get; set; }
 
-			public string MovieImdbRating{ get; set; }
+            public string MovieYear { get; set; }
 
-			public string SubFeatured{ get; set; }
+            public string MovieImdbRating { get; set; }
 
-			public string UserNickName{ get; set; }
+            public string SubFeatured { get; set; }
 
-			public string ISO639{ get; set; }
+            public string UserNickName { get; set; }
 
-			public string LanguageName{ get; set; }
+            public string ISO639 { get; set; }
 
-			public string SubComments{ get; set; }
+            public string LanguageName { get; set; }
 
-			public string SubHearingImpaired{ get; set; }
+            public string SubComments { get; set; }
 
-			public string UserRank{ get; set; }
+            public string SubHearingImpaired { get; set; }
 
-			public string SeriesSeason{ get; set; }
+            public string UserRank { get; set; }
 
-			public string SeriesEpisode{ get; set; }
+            public string SeriesSeason { get; set; }
 
-			public string MovieKind{ get; set; }
+            public string SeriesEpisode { get; set; }
 
-			public string SubDownloadLink{ get; set; }
+            public string MovieKind { get; set; }
 
-			public string ZipDownloadLink{ get; set; }
-		}
+            public string SubDownloadLink { get; set; }
 
-		public  List<SearchResult> ParseSearchResult (Hashtable result)
-		{
+            public string ZipDownloadLink { get; set; }
+        }
 
-			var ret = new List<SearchResult> ();
-			foreach (Hashtable res in (ArrayList)result["data"]) {
-				var p = new SearchResult ();
-				p.MatchedBy = (res ["MatchedBy"] ?? string.Empty).ToString ();
-				p.IDSubMovieFile = (res ["IDSubMovieFile"] ?? string.Empty).ToString ();
-				p.MovieHash = (res ["MovieHash"] ?? string.Empty).ToString ();
-				p.MovieByteSize = (res ["MovieByteSize"] ?? string.Empty).ToString ();
-				p.IDSubtitleFile = (res ["IDSubtitleFile"] ?? string.Empty).ToString ();
-				p.SubActualCD = (res ["SubActualCD"] ?? string.Empty).ToString ();
-				p.SubSize = (res ["SubSize"] ?? string.Empty).ToString ();
-				p.SubHash = (res ["SubHash"] ?? string.Empty).ToString ();
-				p.IDSubtitle = (res ["IDSubtitle"] ?? string.Empty).ToString ();
-				p.SubFileName = (res ["SubFileName"] ?? string.Empty).ToString ();
-				p.UserID = (res ["UserID"] ?? string.Empty).ToString ();
-				p.SubLanguageID = (res ["SubLanguageID"] ?? string.Empty).ToString ();
-				p.SubFormat = (res ["SubFormat"] ?? string.Empty).ToString ();
-				p.SubSumCD = (res ["SubSumCD"] ?? string.Empty).ToString ();
-				p.SubAuthorComment = (res ["SubAuthorComment"] ?? string.Empty).ToString ();
-				p.SubAddDate = DateTime.Parse (res ["SubAddDate"].ToString());
-				p.SubBad = (res ["SubBad"] ?? string.Empty).ToString ();
-				p.SubRating = (res ["SubRating"] ?? string.Empty).ToString ();
-				p.SubDownloadsCnt = (res ["SubDownloadsCnt"] ?? string.Empty).ToString ();
-				p.MovieReleaseName = (res ["MovieReleaseName"] ?? string.Empty).ToString ();
-				p.IDMovie = (res ["IDMovie"] ?? string.Empty).ToString ();
-				p.IDMovieImdb = (res ["IDMovieImdb"] ?? string.Empty).ToString ();
-				p.MovieName = (res ["MovieName"] ?? string.Empty).ToString ();
-				p.MovieNameEng = (res ["MovieNameEng"] ?? string.Empty).ToString ();
-				p.MovieYear = (res ["MovieYear"] ?? string.Empty).ToString ();
-				p.MovieImdbRating = (res ["MovieImdbRating"] ?? string.Empty).ToString ();
-				p.SubFeatured = (res ["SubFeatured"] ?? string.Empty).ToString ();
-				p.UserNickName = (res ["UserNickName"] ?? string.Empty).ToString ();
-				p.ISO639 = (res ["ISO639"] ?? string.Empty).ToString ();
-				p.LanguageName = (res ["LanguageName"] ?? string.Empty).ToString ();
-				p.SubComments = (res ["SubComments"] ?? string.Empty).ToString ();
-				p.SubHearingImpaired = (res ["SubHearingImpaired"] ?? string.Empty).ToString ();
-				p.UserRank = (res ["UserRank"] ?? string.Empty).ToString ();
-				p.SeriesSeason = (res ["SeriesSeason"] ?? string.Empty).ToString ();
-				p.SeriesEpisode = (res ["SeriesEpisode"] ?? string.Empty).ToString ();
-				p.MovieKind = (res ["MovieKind"] ?? string.Empty).ToString ();
-				p.SubDownloadLink = (res ["SubDownloadLink"] ?? string.Empty).ToString ();
-				p.ZipDownloadLink = (res ["ZipDownloadLink"] ?? string.Empty).ToString ();
-				ret.Add (p);
-			}	
-
-			return ret;
-				
-		}
-	}
+        #endregion
+    }
 }
